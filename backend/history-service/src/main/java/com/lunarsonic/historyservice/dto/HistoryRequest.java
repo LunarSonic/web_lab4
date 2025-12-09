b2ecdf14c642a214c6bbee536f58aada282bea0e
Sonya Abdullaeva <sophiaabdullaeva2007@icloud.com>
2025-12-09 18:18:29 +0300

    missing userId added to fix deserialization error

--- backend/history-service/src/main/java/com/lunarsonic/historyservice/dto/HistoryRequest.java
+++ backend/history-service/src/main/java/com/lunarsonic/historyservice/dto/HistoryRequest.java
@@ -1,5 +1,5 @@
 package com.lunarsonic.historyservice.dto;
 import com.lunarsonic.historyservice.model.HistoryActionType;
 
-public record HistoryRequest(HistoryActionType actionType, String payload) {
+public record HistoryRequest(Long userId, HistoryActionType actionType, String payload) {
 }
