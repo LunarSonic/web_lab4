cf54aedb4eecc4c9f79c4e960e3398d341263f4f
Sonya Abdullaeva <sophiaabdullaeva2007@icloud.com>
2025-12-09 13:11:10 +0300

    unnecessary annotation removed

--- backend/history-service/src/main/java/com/lunarsonic/historyservice/dto/HistoryRequest.java
+++ backend/history-service/src/main/java/com/lunarsonic/historyservice/dto/HistoryRequest.java
@@ -1,7 +1,5 @@
 package com.lunarsonic.historyservice.dto;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.lunarsonic.historyservice.model.HistoryActionType;
 
-@JsonIgnoreProperties(ignoreUnknown = true)
 public record HistoryRequest(HistoryActionType actionType, String payload) {
 }
